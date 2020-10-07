07fb515a8fddb94bfe46ce1ed4fa4d87ba13a551
GretaFa <50305596+GretaFa@users.noreply.github.com>
2020-10-07 19:38:03 +0200

    elso_teljes

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const mysql = require(config.dbDriver);
+const express = require('express');
+const app = express();
+const config = require('./config');
 
-export const conn = mysql.createConnection({
+const mysql = require(config.dbDriver);
+const device = require('express-device');
+
+app.use(device.capture());
+app.use(express.static(__dirname));
+//app.use(config.appVersion, router);
+
+app.listen(config.appPort, () => {
+    console.log(`Software running ${config.appPort}...`);
+});
+
+const conn = mysql.createConnection({
     host: config.dbHost,
     user: config.dbUser,
     password: config.dbPassword,
@@ -12,5 +25,8 @@ conn.connect(function(err){
         console.log('Database connection error! Error: ' + err);
     } else {
         console.log(`Connectrion to '${config.dbName}' database sucessful on port ${config.dbPort}!"`);
+        console.log('elso');
     }
 });
+
+module.exports = app;
